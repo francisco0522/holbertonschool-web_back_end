a05e25793d05a9ca8d90bb24f133a9d6ac805a58
Francisco <francisco.londono@fromwolf.com>
2021-04-29 11:43:51 -0500

    Adding task

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Write a coroutine called async_generator that takes no arguments.
-The coroutine will loop 10 times, each time asynchronously wait 1 
+"""The coroutine will loop 10 times, each time asynchronously wait 1 
 second, then yield a random number between 0 and 10. Use the random module.
 """
 import random
@@ -9,8 +8,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Write a coroutine called async_generator that takes no arguments.
-    The coroutine will loop 10 times, each time asynchronously wait 1 
+    """The coroutine will loop 10 times, each time asynchronously wait 1 
     second, then yield a random number between 0 and 10. Use the random module.
     """
     for _ in range(10):
