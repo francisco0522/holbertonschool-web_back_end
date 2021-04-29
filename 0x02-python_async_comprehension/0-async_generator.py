7b1646123afba754e52633b9377a33bd1549c493
Francisco <francisco.londono@fromwolf.com>
2021-04-29 11:47:47 -0500

    Adding task

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-"""The coroutine will loop 10 times, each time 
-asynchronously wait 1 second, then yield a random 
-number between 0 and 10. Use the random module.
-"""
+"""Async Generator"""
 import random
 import asyncio
 from typing import Generator
