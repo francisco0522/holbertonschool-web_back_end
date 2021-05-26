2583448f0948a6bba0176569af74056a0fedd45e
Francisco <francisco.londono@fromwolf.com>
2021-05-26 15:49:23 -0500

    fixing, no str

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -76,7 +76,7 @@ def get_reset_password_token() -> str:
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
-def update_password() -> str:
+def update_password():
     """Update the password"""
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
