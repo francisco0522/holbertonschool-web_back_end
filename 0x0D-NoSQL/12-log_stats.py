675235e2ac2d04944efc265ad431d3eb04a5b440
Francisco <francisco.londono@fromwolf.com>
2021-06-15 22:52:23 -0500

    Adding files

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
 """
-Where can I learn Python?
+    Write a Python function that returns
+    the list of school having a specific topic
 """
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-    number = nginx_collection.count()
-    number_get = nginx_collection.find({"method": "GET"}).count()
-    number_post = nginx_collection.find({"method": "POST"}).count()
-    number_put = nginx_collection.find({"method": "PUT"}).count()
-    number_patch = nginx_collection.find({"method": "PATCH"}).count()
-    number_delete = nginx_collection.find({"method": "DELETE"}).count()
-    number_status = nginx_collection.find(
-        {"method": "GET", "path": "/status"}).count()
 
-    print("{} logs".format(number))
+def logger(a: dict) -> int:
+    """return logger"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_logs = client.logs.nginx
+    return nginx_logs.count_documents(a)
+
+
+def main():
+    """
+    Nginx logs stored in MongoDB
+    """
+    print(f"{ logger({}) } logs")
     print("Methods:")
-    print("\tmethod GET: {}".format(number_get))
-    print("\tmethod POST: {}".format(number_post))
-    print("\tmethod PUT: {}".format(number_put))
-    print("\tmethod PATCH: {}".format(number_patch))
-    print("\tmethod DELETE: {}".format(number_delete))
-    print("{} status check".format(number_status))
+    print(f"\tmethod GET: { logger({'method': 'GET'}) }")
+    print(f"\tmethod POST: { logger({'method': 'POST'}) }")
+    print(f"\tmethod PUT: {logger({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {logger({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {logger({'method': 'DELETE'})}")
+    print(f"{logger({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    main()
